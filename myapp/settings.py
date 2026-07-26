1bb4dedf07adf3d99bbb368f4a521fcb235ec176
hasini-18-ux <pghasini2006@gmail.com>
2026-07-26 06:34:12 +0530

    modified

--- myapp/settings.py
+++ myapp/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'amazon',
-        'USER':'root',
-        'PASSWORD':'hasini@18H',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

b63a00d6acb3058c1105be8e25ff2c9a7db4ed09
hasini-18-ux <pghasini2006@gmail.com>
2026-07-28 09:53:03 +0530

    Added Procfile

--- myapp/settings.py
+++ myapp/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'amazon',
+        'USER':'root',
+        'PASSWORD':'hasini@18H',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
+
 
 
 # Password validation
